660d31c21ef3ae4461b5472a266f245069d04de2
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-12-18 17:58:52 +0400

    Task : Sql operation in Join,Like,Case,Union etc.

--- SqlTask(1).sql
+++ SqlTask(1).sql
@@ -1,4 +1,4 @@
-create database Work
+﻿create database Work
 use Work
 
 create table Customer(
@@ -7,7 +7,7 @@ Customer_Name nvarchar(25) not null,
 Company_Name nvarchar(50)
 )
 
-select *from Customer
+
 
 create table Orders (
 Id int identity primary key,
@@ -26,7 +26,7 @@ Product_Name nvarchar(50) not null,
 Quantity int 
 )
 
-select *from OrderDetails
+
 
 insert into Customer(Customer_Name, Company_Name)
 values ('Vagif', 'SINAM');
@@ -58,4 +58,73 @@ values
 (4, 'TV',20),
 (1, 'AirPods',200);
 
+
+select *from OrderDetails
+select Customer_Name from Customer
+
+
+select order_d.*,cst.Company_Name from OrderDetails order_d inner join  Orders ord on order_d.Id=ord.Id
+inner join Customer cst on ord.Customer_ID=cst.Id
+
+
+---Bütün customerləri və onların sifarişlərini(orderlərini və məlumatlarını) əgər customerin heç bir sifarişi yoxdursa onlarıda gətir
+
+select cs.Customer_Name,ord.* from Customer cs left join Orders ord on cs.Id=ord.Customer_ID
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DepartmentID INT,
+    Title VARCHAR(50)
+);
+
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
+);
+
+CREATE TABLE Projects (
+    ProjectID INT PRIMARY KEY,
+    ProjectName VARCHAR(50),
+    EmployeeID INT
+);
+
+
+INSERT INTO Employees (EmployeeID, FirstName, LastName, DepartmentID, Title) VALUES
+    (1, 'Aydın', 'Məmmədov', 1, 'Software Developer'),
+    (2, 'Əbdül', 'Kərimov', 2, 'HR Specialist'),
+    (3, 'Kamil', 'Qarayev', 1, 'Database Administrator'),
+    (4, 'Səbuhi', 'Culfayev', 2, 'HR Manager'),
+    (5, 'Vəli', 'Hacıyev', 3, 'IT Specialist');
+
+INSERT INTO Departments (DepartmentID, DepartmentName) VALUES
+    (1, 'IT'),
+    (2, 'HR'),
+    (3, 'Finance');
+
+INSERT INTO Projects (ProjectID, ProjectName, EmployeeID) VALUES
+    (1, 'ProjectA', 1),
+    (2, 'ProjectB', 2),
+    (3, 'ProjectC', 4);
+
+
+
+---İşçilər və şöbələr arasında əlaqəni tapın və  işçi adlarını tərs sıralayın.
+---Ayrıca, "Developer" adlı vəzifəyə sahib olan işçiləri seçin və "Projects" cədvəli ilə əlaqələndirin.
+
+
+select emp.FirstName,emp.LastName,emp.Title,dpr.DepartmentName from Employees emp inner join Departments dpr on emp.DepartmentID=dpr.DepartmentID order by emp.FirstName DESC
+
+select emp.FirstName,emp.LastName,emp.Title,prj.ProjectName from Employees emp inner join Projects prj  on emp.EmployeeID=prj.EmployeeID  where emp.Title like '%Developer%';
 
--- SqlTask(2).sql
+++ SqlTask(2).sql
@@ -44,3 +44,84 @@ select * from STUDENT where Name like 'E%' and Name like '%n';
 
 
 select CONCAT(Name,'  ',Surname) as Fulname from  STUDENT
+
+
+Update STUDENT set STUDENT.Name='Mehemmed' where STUDENT.Name='Vagif'and STUDENT.Surname='Garayev'and STUDENT.Age=19;
+
+Delete STUDENT where STUDENT.Name='Elxan' and STUDENT.Surname='Garayev'
+
+select *from STUDENT
+
+create table StudentInfo(
+Id int identity Primary KEy,
+Name nvarchar(25) Not NUll,
+Surname nvarchar(25) not null,
+Age int NOt Null,
+Birthday date Not null
+);
+
+
+ 
+
+
+
+create table StudentGrade(
+Student_ID int references StudentInfo(Id),
+Grade int Not null,
+GradeABC nvarchar(1) not null
+);
+
+
+
+
+insert into StudentInfo (Name,Surname,Age,Birthday)
+values
+('Vagif','Garayev',19,'2004/10/12'),
+('Arif','Garayev',10,'2013/10/12'),
+('Murad','Garayev',20,'2003/10/12'),
+('Nizami','Garayev',25,'1998/10/12')
+
+select * from StudentInfo
+
+insert into StudentGrade (Student_ID,Grade,GradeABC)
+values 
+(2,80,'B'),
+(4,90,'A'),
+(1,70,'C'),
+(3,75,'C');
+
+
+select *from StudentGrade
+
+
+select si.*,sg.Grade,sg.GradeABC from StudentInfo si inner join StudentGrade sg on si.Id=sg.Student_ID
+
+
+select *from STUDENT Order by Age DESC;
+select *from STUDENT Order by Age ASC;
+
+select Min(Age) from STUDENT;
+
+select Name,Surname, Min(Age) from STUDENT group by Name,Surname
+
+select * from STUDENT where Age =(Select Min(Age) from STUDENT)
+
+select Count(Name) from STUDENT
+
+select *from StudentInfo where Name in ('Vagif','Arif') 
+
+
+select Age from StudentInfo
+union All
+select Grade from StudentGrade
+
+select all Name from STUDENT where Name='Arif'
+
+select Age,
+CASE 
+     when Age<10 then 'He is Child'
+	 when Age>10 then 'He is big boy'
+
+  else 'He is man'
+END as Situation
+from StudentInfo
